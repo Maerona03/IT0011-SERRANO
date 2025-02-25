33f1bbf2f14ca85add80360f5f85254f00368f06
Rona Mae Serrano <167689876+Maerona03@users.noreply.github.com>
2025-02-25 18:55:55 +0800

    Update 3-4.py

--- Activity_03/3-4.py
+++ Activity_03/3-4.py
@@ -1,11 +1,11 @@
 try:
-    with open("students.txt", "r") as file:
-      student_info = file.read().strip()
+    with open("students.txt", "r") as file: #Open the "students.txt" file in read mode
+      student_info = file.read().strip() # Read the entire content of the file and remove leading/trailing whitespace
     
-    if not student_info:
+    if not student_info: # Check if the file is empty
         print("No student information found in 'students.txt'.")
-    else:
+    else:  # Display the student information
         print("\nReading student Information:")
         print(student_info)
-except FileNotFoundError:
+except FileNotFoundError: # Handle the case where the file does not exist
     print("No student information found in 'students.txt'.")
